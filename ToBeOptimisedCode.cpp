2ef40dbf3620fd2ea55be393749e23f07acab818
Sean Sim <eeyss19@nottingham.ac.uk>
2024-11-11 20:03:50 +0000

    Direct access via struct  < 5.5s

--- ToBeOptimisedCode.cpp
+++ ToBeOptimisedCode.cpp
@@ -4,112 +4,83 @@
 #include <sstream>
 #include <sys/time.h>
 #include <cstring>
+#include <algorithm>
 
 using namespace std;
 
 
+struct baseArgs{
+        static constexpr int nx = 10000;                                            // Changed const, could use a typedef here?
+        static constexpr int ny = 200;                                              // Not sure if I like this 
+        static constexpr int nt = 200;                                              // Changed to 200 to allow for more testing
+};
+
+struct countersAndScalers {
+        static constexpr double dividebyfour = 0.25;
+        static constexpr double dividebytwo = 0.5;
+        int i, j, t;
+};
+
+struct array2D {
+    double** vi = new double*[baseArgs::nx];
+    double** vr = new double*[baseArgs::nx];
+};
+
 int main(int argc, char* argv[]) {
 
-        struct timeval start_time,end_time;
+        baseArgs args;
+        array2D arrays;
+        countersAndScalers cs;
 
-        const int nx(10000);            // Changed const, could use a typedef here?
-
-        const int ny(200);
-
-        const int nt(200);
-
-        double** vi=new double*[nx];
-
-        double** vr=new double*[nx];
-
-        const double pi=(4.*atan(1.));
-
-        for(int i=0;i<nx;++i) {
-
-                vi[i]=new double[ny];           // Creating the 2D array here
-                // std::memset(a,0.,ny*sizeof(double)); // Not applicable here
-                vr[i]=new double[ny];
-
+        const double pi= M_PI;
+        for(cs.i=0;cs.i<args.nx;++cs.i) {
+                arrays.vi[cs.i]=new double[args.ny];                                    // Creating the 2D array here
+                arrays.vr[cs.i]=new double[args.ny];
         }
         
-        const double pi_over_nx = pi/double(nx);     
-        // gettimeofday(&start_time,NULL);    
-        for(int i=0;i<nx;++i) {
+        const double pi_over_nx = pi/double(args.nx);     
+          
+        for(cs.i=0;cs.i<args.nx;++cs.i) {
+                const double constant = double(cs.i*cs.i)*sin(pi_over_nx*double(cs.i));
+                for(cs.j=0;cs.j<args.ny;++cs.j) {
 
-                for(int j=0;j<ny;++j) {
-
-                        vi[i][j]=double(i*i)*double(j)*sin(pi_over_nx*double(i));
-
-                        memset(&vr[i][j],0,sizeof(double));         // Ah why not eh? Seems to perform identically to the line below so I'm keeping it
-                        // vr[i][j] = 0;           // Testing against memset
+                        arrays.vi[cs.i][cs.j]=constant * double(cs.j);         
+                        memset(&arrays.vr[cs.i][cs.j],0,sizeof(double));                // Ah why not eh? Seems to perform identically to the line below so I'm keeping it
+                                                                                        // vr[i][j] = 0;           // Testing against memset
                 }
         }
-        gettimeofday(&end_time,NULL);
-        // cout<<"\nInitialisation time (s) = "<<end_time.tv_sec-start_time.tv_sec+(end_time.tv_usec-start_time.tv_usec)/1e6;
-        // memset(vr,0,nx*ny*sizeof(double));           Not possible as we are using a 2D array here, no guarantee that the memory is contiguous          
-        // Could look to sort the data here to demostrate awareness of branch prediction
+        
+                                                                                        // memset(vr,0,nx*ny*sizeof(double));           Not possible as we are using a 2D array here, no guarantee that the memory is contiguous          
+                                                                                        // Could look to sort the data here to demostrate awareness of branch prediction
 
         ofstream fout("data_out");
-        const double dividebyfour(0.25);
-        const double dividebytwo(0.5);
-        
-        for(int t=0;t<nt;++t) {         // Will try and test against zero here as it is not used for array access, so we will not be interfering with sequential cache access, probably not allowed as it changes the logic of the code
-                                        // Will try and make small modification to permit the above modification without changing the output of the code
-                cout<<"\n"<<t;cout.flush();             // Changed the logic to (nt-t) to allow for expected output, doesnt seem to increase runtime
-                                                        // Adds new lines to the output data, abandoned this idea as its not worth the effort
-                for(int i=0;i<nx;++i) {
-
-                        for(int j=0;j<ny;++j) {
-
-                                if(i > 0 && i < nx-1 && j > 0 && j < ny-1) {            // The idea is that if one of these tests fails then we can isolate exactly which of the two conditions if failed.
-
-                                        vr[i][j]=(vi[i+1][j]+vi[i-1][j]+vi[i][j-1]+vi[i][j+1])*dividebyfour;              // Tried to change i+1 to use increment operator but practically halts the runtime, why is that?
-
-                                } else if(i == 0 && j > 0 && j < ny-1) {     // Redundant condition, could be removed to save time
-
-                                        vr[i][j]=(vi[i+1][j]+10.+vi[i][j-1]+vi[i][j+1])*dividebyfour;                     // All of these division operations could be replaced with a bitshift operation, but the compiler will probably do this for us
-                                                                                                                // Will try it anyway? Nevermind its a double, so it will not work
-                                } else if(i == nx-1 && j > 0 && j < ny-1) {          // Again repeated conditional, why check if by being at this point we know the result of that check anyway                        // Yet still faster to multiply by 0.25, managed to get below 6 seconds here !
-
-                                        vr[i][j]=(5.+vi[i-1][j]+vi[i][j-1]+vi[i][j+1])*dividebyfour;            // Would a switch statement be faster than if/else? 
-
-                                } else if(j == 0 && i > 0 && i < nx-1) {
-
-                                        vr[i][j]=(vi[i+1][j]+vi[i-1][j]+15.45+vi[i][j+1])*dividebyfour;
-
-                                } else if(j == ny-1 && i > 0 && i < nx-1) {
-
-                                        vr[i][j]=(vi[i+1][j]+vi[i-1][j]+vi[i][j-1]-6.7)*dividebyfour;
-
-                                }
+                
+        for(cs.t=0;cs.t<args.nt;++cs.t) {                                               // Will try and test against zero here as it is not used for array access, so we will not be interfering with sequential cache access, probably not allowed as it changes the logic of the code
+                                                                                        // Will try and make small modification to permit the above modification without changing the output of the code
+                cout<<"\n"<<cs.t;cout.flush();                                          // Changed the logic to (nt-t) to allow for expected output, doesnt seem to increase runtime
+                                                                                        // Adds new lines to the output data, abandoned this idea as its not worth the effort
+                for(cs.i=1;cs.i<args.nx-1;cs.i++) {
+                        arrays.vr[cs.i][0]=(arrays.vi[cs.i+1][0]+arrays.vi[cs.i-1][0]+15.45+arrays.vi[cs.i][1])*cs.dividebyfour;
+                        arrays.vr[cs.i][199]=(arrays.vi[cs.i+1][199]+arrays.vi[cs.i-1][199]+arrays.vi[cs.i][198]-6.7)*cs.dividebyfour;
+                }
+                for(cs.i=1;cs.i<args.nx-1;cs.i++) {
+                        for(cs.j=1;cs.j<args.ny-1;cs.j++) {
+                                arrays.vr[cs.i][cs.j]=(arrays.vi[cs.i+1][cs.j]+arrays.vi[cs.i-1][cs.j]+arrays.vi[cs.i][cs.j-1]+arrays.vi[cs.i][cs.j+1])*cs.dividebyfour;
                         }
                 }
-                // Loop unrolling for these final two loops could be beneficial
-                
-                for(int i=0;i<nx;++i) {
-
-                        for(int j=0;j<ny;++j) {           // Tried loop unrolling but the duplication of if statements kills the runtime
-
-                                if(fabs(fabs(vr[i][j])-fabs(vi[i][j]))<1e-2) fout<<"\n"<<t<<" "<<i<<" "<<j<<" "<<fabs(vi[i][j])<<" "<<fabs(vr[i][j]); 
-                        }
+                for(cs.j=1;cs.j<args.ny-1;cs.j++) {
+                        arrays.vr[0][cs.j]=(arrays.vi[1][cs.j]+10.+arrays.vi[0][cs.j-1]+arrays.vi[0][cs.j+1])*cs.dividebyfour;
+                        arrays.vr[9999][cs.j]=(5.+arrays.vi[9998][cs.j]+arrays.vi[9999][cs.j-1]+arrays.vi[9999][cs.j+1])*cs.dividebyfour;
                 }
                 
-                 
-                for(int i=0;i<nx;++i) {              // Merged these two loops into one, seems to be noticably faster
+                for(cs.i=0;cs.i<args.nx;++cs.i) {
 
-                        for(int j=0;j<ny;j+=8) {        // Loop unrolling 200 % 8 = 0, so we can unroll. Also 8 doubles to a cache line so hopefully this will be faster
-                        
-                        //  vi[i][j]=(vi[i][j]*dividebytwo)+(vr[i][j]*dividebytwo);         
-
-                        vi[i][j]=(vi[i][j]*dividebytwo)+(vr[i][j]*dividebytwo);         // Loop unrolling may be slightly faster, code may be slightly less readable but I think it shows awareness of cache performance
-                        vi[i][j+1]=(vi[i][j+1]*dividebytwo)+(vr[i][j+1]*dividebytwo);
-                        vi[i][j+2]=(vi[i][j+2]*dividebytwo)+(vr[i][j+2]*dividebytwo);
-                        vi[i][j+3]=(vi[i][j+3]*dividebytwo)+(vr[i][j+3]*dividebytwo);
-                        vi[i][j+4]=(vi[i][j+4]*dividebytwo)+(vr[i][j+4]*dividebytwo);
-                        vi[i][j+5]=(vi[i][j+5]*dividebytwo)+(vr[i][j+5]*dividebytwo);
-                        vi[i][j+6]=(vi[i][j+6]*dividebytwo)+(vr[i][j+6]*dividebytwo);
-                        vi[i][j+7]=(vi[i][j+7]*dividebytwo)+(vr[i][j+7]*dividebytwo);
-
+                        for(cs.j=0;cs.j<args.ny;++cs.j) {                               // Tried loop unrolling but the duplication of if statements kills the runtime
+                                                                                        // Calculating the absolute values slowed it by almsot a second
+                                const double valvr(fabs(arrays.vr[cs.i][cs.j]));
+                                const double valvi(fabs(arrays.vi[cs.i][cs.j]));
+                                if(fabs(valvr-valvi)<1e-2) fout<<"\n"<<cs.t<<" "<<cs.i<<" "<<cs.j<<" "<<valvi<<" "<<valvr; 
+                                arrays.vi[cs.i][cs.j]=(arrays.vi[cs.i][cs.j]*cs.dividebytwo)+(arrays.vr[cs.i][cs.j]*cs.dividebytwo);
                         }
                 }
         }
