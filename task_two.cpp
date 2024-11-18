6c7e11bd397801c986e2e6cb8bea3194079ac4b5
Sean Sim <eeyss19@nottingham.ac.uk>
2024-11-18 00:49:49 +0000

    Forgot to stage part two changes

--- task_two.cpp
+++ task_two.cpp
@@ -8,8 +8,6 @@ struct timeval start_time, end_time;
 
 using namespace std;
 
-
-
 double i_dependent_fn(int i){       // Dependent on i to simulate operations not equal in effort, encourages the use of dynamic scheduling
     double result = .0;
     for(int j = 0 ; j < (i % 1000) * 10 ; ++j){
@@ -19,25 +17,29 @@ double i_dependent_fn(int i){       // Dependent on i to simulate operations not
 }
 
 int main() {
-    std::ofstream outfile("task_two_output_offset1408kB.csv");
-    outfile << "Offset,Runtime (s),Runtime Difference (s)\n";
+    std::ofstream outfile("task_two_output_offset_2dStaticVsDynamic.csv");
+    // outfile << "Offset,Runtime (s),Runtime Difference (s)\n";
+    // outfile << "Memory (kB),Runtime (s),Runtime Difference (s)\n";
+    outfile << "Memory (kB),Static Runtime (s),Dynamic Runtime (s)\n";
     const int max_offset = 10;
     // const int max_n(4*1024*1024);
     const int offset(0);
-    const int num_repeats(10); 
+    const int num_repeats(1); // Changed from 10 -> 100
 
     double total_time_static(0.0);
-    // double total_time_dynamic(0.0);
+    double total_time_dynamic(0.0);
 
-    // omp_set_num_threads(6);
+    omp_set_num_threads(6);
 
-    const int n = 180224;
+    // const int n = 1216000;
+    int n;
     int i, repeat;
     double last_time = 0.0;
     
-    // for (n = 1024; n <= max_n; n*=2) { 
     // for (n = 0; n <= 2000000; n += (n < 49152 ? 512 : n < 196608 ? 4096 : n < 1572864 ? 16384 : 65536)) {
-    for(int offset = 0; offset < max_offset; offset++) {
+    // #pragma omp parallel for private(n, i, repeat, start_time, end_time) reduction(+:total_time_static, total_time_dynamic) default(shared)
+    for (n = 500 ; n < 2000000 ; n*=2) { 
+    // for(int offset = 0; offset < max_offset; offset++) {
         double *a = new double[n];
         double *b = new double[n];
         double *c = new double[n + offset];
@@ -47,12 +49,12 @@ int main() {
         }
 
         gettimeofday(&start_time, NULL); 
-        // #pragma omp parallel for schedule(static) private(i, repeat) default(shared)
+        #pragma omp parallel for schedule(static) collapse(2) private(i, repeat) default(shared)
         for (repeat = 0 ; repeat < num_repeats; repeat++) {
                 // #pragma omp parallel for schedule(static) private(i)
                 for (i = 0; i < (n - offset); i++) {
-                    a[i] = b[i] + c[i + offset];
-                    // a[i] = b[i] + c[i + offset] + i_dependent_fn(i); 
+                    // a[i] = b[i] + c[i + offset];
+                    a[i] = b[i] + c[i + offset] + i_dependent_fn(i); 
                     }
                 }
 
@@ -64,40 +66,43 @@ int main() {
         
         cout<< "Static scheduling: for n = " << n << " and offset = " << offset <<", average wall time = " << time_per_iteration_static << " seconds" << endl;
         total_time_static += time_per_iteration_static;
-        double time_per_iteration_static_difference = time_per_iteration_static - last_time;
-        last_time = time_per_iteration_static;
+        // double time_per_iteration_static_difference = time_per_iteration_static - last_time;
+        // last_time = time_per_iteration_static;
 
         // n is the amount of doubles per array, 8 bytes per double, n->kB = n * 8 / 1024
-        double n_kB = n * 8 / 1024.0;
         // outfile << n_kB << "," << time_per_iteration_static<< "," << time_per_iteration_static_difference << "\n";
-        outfile << offset << "," << time_per_iteration_static<< "," << time_per_iteration_static_difference << "\n";
+        // outfile << offset << "," << time_per_iteration_static<< "," << time_per_iteration_static_difference << "\n";
 
+        gettimeofday(&start_time, NULL);
+        #pragma omp parallel for schedule(dynamic) collapse(2) private(i, repeat) default(shared)
+        for (int repeat = 0 ; repeat < num_repeats; repeat++) {
+                for (i = 0; i < (n - offset); i++) {
+                    // a[i] = b[i] + c[i + offset];
+                    a[i] = b[i] + c[i + offset] + i_dependent_fn(i); 
+                    }
+                }
+        gettimeofday(&end_time, NULL); 
 
+        double elapsed_time_dynamic = (end_time.tv_sec - start_time.tv_sec) +
+                                      (end_time.tv_usec - start_time.tv_usec) / 1e6;
+        double time_per_iteration_dynamic = elapsed_time_dynamic / num_repeats;
+        cout<< "Dynamic scheduling: for n = " << n << " and offset = " << offset <<", average wall time = " << time_per_iteration_dynamic << " seconds" << endl;
+        total_time_dynamic += time_per_iteration_dynamic;
 
-        // gettimeofday(&start_time, NULL);
-        // for (int repeat = 0 ; repeat < num_repeats; repeat++) {
-        //         #pragma omp parallel for schedule(dynamic, 100) private(i)
-        //         for (i = 0; i < (n - offset); i++) {
-        //             // a[i] = b[i] + c[i + offset];
-        //             a[i] = b[i] + c[i + offset] + i_dependent_fn(i); 
-        //             }
+        double n_kB = n * 8 / 1024.0;
+        outfile << n_kB << "," << time_per_iteration_static << "," << time_per_iteration_dynamic << "\n";
 
-        //         }
-        // gettimeofday(&end_time, NULL); 
-
-        // double elapsed_time_dynamic = (end_time.tv_sec - start_time.tv_sec) +
-        //                               (end_time.tv_usec - start_time.tv_usec) / 1e6;
-        // double time_per_iteration_dynamic = elapsed_time_dynamic / num_repeats;
-        // cout<< "Dynamic scheduling: for n = " << n << " and offset = " << offset <<", average wall time = " << elapsed_time_dynamic << " seconds" << endl;
-        // total_time_dynamic += time_per_iteration_dynamic;
 
         delete[] a;
         delete[] b;
         delete[] c;
     }
     outfile.close();
-    // cout<<"Total time with dynamic scheduling = " << total_time_dynamic << " seconds" << endl;
-    // cout<<"Total time with static scheduling = " << total_time_static * 1000 << " milliseconds" << endl;
+    cout<<"Total time with dynamic scheduling = " << total_time_dynamic << " seconds" << endl;
+    cout<<"Total time with static scheduling = " << total_time_static << " seconds" << endl;
 
     return 0;
 }
+
+
+
